875207471538db9f699e61b4a0a1fbb8638b678c
Sumit <sumitgoswami9916@gmail.com>
2022-10-30 22:49:29 +0530

    Reverse natural number

---
+++ numbers_loops.py
@@ -0,0 +1,14 @@
+# print first 10 natural numbers in reverse using while loop
+#  and skip value 5.
+a = 11
+while 0<a<=11:
+    a -= 1
+    if a == 5:
+        continue
+    print(a)
+    
+
+    
+
+
+
