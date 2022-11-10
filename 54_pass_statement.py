8a58fee601a1fdfd3121a4be746a5eea75756dfb
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-10 23:24:02 +0530

    Pass Statement

---
+++ 54_pass_statement.py
@@ -0,0 +1,27 @@
+#pass statement ----> pass is a null statement in Python .It instructs to  "Do nothing"
+
+
+#example
+i = 4
+if i>0:
+   pass # without pass its shows error
+print("we are coders")    
+
+
+
+# example
+i = 4
+if i>0:
+   pass # without pass its shows error
+while i>6:
+    pass
+print("we are coders")  
+
+
+
+
+
+
+
+
+
