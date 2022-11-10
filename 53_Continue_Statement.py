0e7a6b1b9c63da344b90bf8e49a3c8dbcc950b75
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-10 23:19:34 +0530

    Continue Statement

---
+++ 53_Continue_Statement.py
@@ -0,0 +1,29 @@
+# continue --Its like cousin of break
+
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+
+#we can skip the number by type 'continue'' 
+for i in range(20):
+    if i == (10):
+        continue
+    print(i)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
