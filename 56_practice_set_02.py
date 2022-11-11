f96bd0ffa6a88499017f8dae456ca467c8de377d
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-11 23:57:40 +0530

    practice_set02

---
+++ 56_practice_set_02.py
@@ -0,0 +1,29 @@
+# Write a program to greet all the person names stored in a list L1 and which start with S
+#                 L1 = ["Harry" ,"Satyam" , "Sachin" , "Rahul"]
+
+l1 = ["Harry" ,"Satyam" ,"Sachin" , "Rahul"]
+for name in l1:
+    if name.startswith("S"):
+        print("Hello" + name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
