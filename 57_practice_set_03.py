7fd0f8864eb6e90a7a36de0ba3d47de8a845bd3a
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-11 23:58:54 +0530

    practice_continue

---
+++ 57_practice_set_03.py
@@ -0,0 +1,24 @@
+# Attempt problem 1 using while loop 
+# write a program to print multiplication table of a given number using while loop
+num = int(input("Enter the number :"))
+i = 1
+while  i <= 10:
+     
+    print( num , " * " , i , " = " ,(num*i)   )
+    i= i+1  
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
