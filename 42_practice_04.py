dae6bb86bad6ce03f2ec39b115c3a0cb729c78e3
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-13 23:58:34 +0530

    practice_question

---
+++ 42_practice_04.py
@@ -0,0 +1,8 @@
+# Write a program to find whether a given username contains  less than  10 character  or not .
+
+ 
+name = input("Enter your name")    
+if len(name) < 10 :
+    print("correct")
+else:
+    print("Your name is too long")    
