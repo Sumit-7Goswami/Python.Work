d6d6d66eae1aa49131fb88a49e1c7a3aeed9731c
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-15 16:34:11 +0530

    Question_Practice

---
+++ 41_practice_03.py
@@ -0,0 +1,26 @@
+
+#Question----> A spam comment is defined as a text containing following keywords :
+##         "make a lot of money", "buy now" , "Subscibe this" , 
+##          "click this" .Write a program to detect these spams.
+
+
+ 
+
+
+text = input("Enter the text")
+
+if("make a lot of money" in text):
+    spam = True 
+elif("buy now" in text):
+    spam = True
+elif("click this" in text):
+    spam = True
+elif("subscribe this" in text):
+    spam = True
+else:
+    spam = False    
+
+if(spam):
+    print("This text is spam")
+else:
+    print("This text is not spam")
