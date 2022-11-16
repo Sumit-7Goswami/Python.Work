95975b7d8264927c75f51bf02e571eb3c56847ca
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-16 23:40:52 +0530

    While Loop

---
+++ 46_01_While_Loop.py
@@ -0,0 +1,22 @@
+i = 10
+while i<20:
+    print("i")
+    i = i + 1
+print("Done")    
+
+
+#another example for writing  while loop
+
+i = 0 
+while i<10:
+    print("Yes" + str(i))
+    i = i + 1
+    
+print("Done")    
+
+""" while loop mai condition check hoti hai jaise kii hme
+condition true he put krna hota hai ...like if we put the
+value of i is 11 ..and we do i<10 ..so its not execute 
+thats why we put true condtion..and after run it ,jab ye condition
+false nhi hoo jati tbb tkk run hoti rheti hh..jab false ho jayga 
+tbb ye code run over o jayga"""
