33c4e7b62c7e2a90db8771af97bc605ce294df48
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-16 23:44:25 +0530

    Quiz loop

---
+++ 47_quick_quiz.py
@@ -0,0 +1,23 @@
+# write a program to print 1 to 50 using a while loop.
+
+i = 1
+while i<=50:
+    print(i) # if we run this code ,to yee 1 infinite tkk print hoga ,mtlb rukega nhi saala
+    i = i + 1 #so apnn ye step add krenge , mtlbb 1 to 50 run hoga ho,bina ruke
+
+
+# an example
+i = 0 
+while i < 5 :
+    print("Sumit") # print 'Sumit' 5 times
+    i = i+1
+
+'''if the condition never becomes False ,the loop keeps getting executed'''    
+
+
+
+
+
+
+
+
