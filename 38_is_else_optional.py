c7634617662242401f6c4145363f370a7c3c3767
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-17 23:46:16 +0530

    is Else

---
+++ 38_is_else_optional.py
@@ -0,0 +1,22 @@
+'''is "else" optional we will see in below code'''
+a = 17
+if(a==7):
+    print("Yes")
+elif(a>23):
+    print("Yes and No") #   if we run , to ye koi syntax nhi dee rha hai
+
+##
+a = 7
+if(a==7):
+    print("Yes")
+elif(a>50):
+        print("no and yes")
+
+##  
+a = 36
+if(a>66):
+    print("Ya")
+elif(a<33):
+    print("Na")
+else:
+    print("I am optional")
