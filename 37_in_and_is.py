779129d4f6efcf6b30f6d5928b871983259cf4b6
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-18 23:40:57 +0530

    in is statement

---
+++ 37_in_and_is.py
@@ -0,0 +1,18 @@
+#   'is' kk baare mai
+a = None
+if (a is None):
+    print("yes")
+else:
+    print("No")    
+
+
+# 'in' kkk baare mm
+a = [45,56,6]
+print(45 in a)
+
+a = [45,56,6]
+print(145 in a)
+''' "in" ye btata hai kii 145 list mm hh yaa nhi ,...like true or false  '''
+
+
+
