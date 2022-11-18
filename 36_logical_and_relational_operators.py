ce266cd361c6bf66b8d3011168a89a08cf68360a
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-18 23:35:35 +0530

    Operators

---
+++ 36_logical_and_relational_operators.py
@@ -0,0 +1,18 @@
+age = 45
+if(age>34 and age<56):
+    print("You can work with us")
+
+else:
+    print("You cannot work with us")   
+
+
+#
+'''ab user se krva kk dekhta he'''
+
+
+age = int(input("Enter your age:  "))
+if(age>34 and age<56):
+    print("You can work with us")
+
+else:
+    print("You cannot work with us")    
