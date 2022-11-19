4953d19227ca113014f533b2f174e79e737fe724
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-19 23:15:56 +0530

    Conditional statement

---
+++ 34_conditionals.py
@@ -0,0 +1,75 @@
+### 1. if-elif-else ladder in python
+
+
+a = 45 
+
+if(a>3):
+    print("The value of a is greater than 3")
+elif(a<3):
+    print("The value of a is greater than 7")
+else:
+    print("The value is not greater than 3 or 7")    
+
+
+
+## 
+a = 45 
+
+if(a>3):
+    print("The value of a is greater than 3")
+elif(a>13):
+    print("The value of a is greater than 13")
+elif(a>7):
+    print("The value of a is greater than 7")
+elif(a>17):
+    print("The value is not greater than 17")
+else:
+    print("The value is not greater than 3 or 7")
+
+
+## 
+
+a = 8 
+if(a<3):
+    print("The value of a greater than 3 ")
+elif(a>13):
+    print("The value of a is greater than 13")
+elif(a>7):
+    print("The value of a is greater than 7")
+elif(a>17):
+    print("The value of a is greater than 17")
+else:
+    print("The value of a is greater than 3 or 7")
+     
+print("Done")     
+
+
+
+### 2. Multiple-If statement
+
+a = 8 
+if(a<3):
+    print("The value of a greater than 3 ")
+if(a>13):
+    print("The value of a is greater than 13")
+if(a>7):
+    print("The value of a is greater than 7")
+if(a>17):
+    print("The value of a is greater than 17")
+else:
+    print("The value of a is greater than 3 or 7")
+     
+print("Done")     
+
+
+
+
+
+
+
+
+
+
+
+
+
