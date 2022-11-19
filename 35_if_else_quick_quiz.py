6cd95025f63b03d55f023f00cab37c89040369c1
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-19 23:16:45 +0530

    If else --- QUIZ

---
+++ 35_if_else_quick_quiz.py
@@ -0,0 +1,13 @@
+# Q--> Write a program to print yes when the age entered by thr user is greater than or equal to 18.print
+
+from xml.sax.handler import feature_validation
+
+
+age = int(input("Enter your age : "))
+
+if age>=18:
+   print("Yes")
+else:
+    print("No")
+    
+ 
