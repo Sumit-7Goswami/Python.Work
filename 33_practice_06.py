9ccffac6c517778f214ce47b8ae034b95516d25b
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-20 23:21:54 +0530

    practice questions

---
+++ 33_practice_06.py
@@ -0,0 +1,9 @@
+# Can you change the values inside a list which is contained in set S 
+#      s = { 8,7,12,"Harry",[1,2] }   
+
+
+'''if interview ask this wrong question to u ,then tell him that in the 
+given SET ,how a list is come in it , as we know a 'list' in "set" is not
+allowed or agrr set mee tupple hota too ..we know tupple to khbi change he nhi ho
+skta '''
+'''Thank u'''
