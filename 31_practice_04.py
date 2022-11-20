fb0316bc9157259d42fd00b557dfec2b2d41fdd6
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-20 23:24:13 +0530

    Question test

---
+++ 31_practice_04.py
@@ -0,0 +1,27 @@
+# what will we be the length of the following set S :
+#       S = set()
+#     S.add (20)
+#     S.add(20.0)
+#     S.add("20")
+#                    '''lenght of S after the operation '''
+
+
+s = {20,20.0,"20"}
+print(len(s))
+
+# we get 2 ,because in python ,set mee 20 or 20.0 ekk barr count hoga bss 
+
+
+
+#### 
+#### QUESTION--->   S = {} , what is the type of S ???
+
+#              answer '''Here S is a type of dictionary not set ,on seeing 
+     #                     it is lool like null set ,but it dictionary set , null set is 
+      #                   written like this S = Set()''''''       
+'''for verify ,go to terminal, write python ,after that ,
+     write s = {}   and  type() ,then enter this and u get class'dict' '''
+
+
+     
+                 
