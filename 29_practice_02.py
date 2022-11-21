b767eedea75da78a0d36deb5ab27d9baff0b5826
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-21 23:52:12 +0530

    practice questions

---
+++ 29_practice_02.py
@@ -0,0 +1,15 @@
+# Write a program to input eight numbers from the user and display all the unique number(once) 
+'''whenever you see unique ,then u must remeber that it talk about list in python'''
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+num3 = int(input("Enter number 3"))
+num4 = int(input("Enter number 4"))
+num5 = int(input("Enter number 5"))
+num6 = int(input("Enter number 6"))
+num7 = int(input("Enter number 7"))
+num8 = int(input("Enter number 8"))
+
+s = {num1 ,num2 ,num3 ,num4 ,num5, num6,num7,num8}
+print(s)
+
+''' \n ye be use krr skte ho ,thoda space ho jayga jbb code run hoga tbb ,ache se dekh skte hh fir'''
