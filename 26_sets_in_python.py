2e2793e51a1a352bf25570f1de1d51be77ea3e33
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-11-22 23:40:57 +0530

    Sets

---
+++ 26_sets_in_python.py
@@ -0,0 +1,11 @@
+# Set is a collection of non-repetative elements
+a =  {1,3,4,5,1}
+print(type(a))
+print(a)
+
+# Important: This syntax will create an empty dictionary and not an empty set 
+a ={}
+print(type(a))
+
+
+ 
