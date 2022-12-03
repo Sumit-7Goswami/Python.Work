1380b9d2e59a653e8c25a86e7e26cb43335f0a5c
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-03 23:32:35 +0530

    List Methods

---
+++ 19_List_method.py
@@ -0,0 +1,46 @@
+# l1.sort() sorts the list means ascending to descending 
+l1 = [1, 8, 7, 2, 21, 15]
+l1.sort()
+print(l1)
+
+'''another example'''
+l1 = [1,8,7,2,21,15]
+print(l1)
+l1.sort()
+print(l1)
+
+
+# l1.reverse() ,reverse the list
+l1 = [4,1,3,8,6,7]
+l1.reverse()
+print(l1)
+
+# l1.append() ---> adds at the end of the list
+l1 = [4,1,3,8,6,7]
+l1.append(21) #adds 21 at the end of the lists.
+print(l1)
+
+
+# l1.insert()
+l1 = [4,1,3,8,6,7]
+l1.insert(0,31) #inserts 31 at  index 0 
+print(l1)
+
+# l1.pop()
+l1 = [4,1,3,8,6,7]
+l1.pop(4) # jo be index number 4 prr h , vo nikal jayga 
+print(l1)
+'''see after run this above code we will see that 
+6 is remove from the list'''
+
+# l1.remove()
+l1 = [4,1,21,8,6,7]
+l1.remove(21) #remove 3 from the lists 
+print(l1)
+
+
+
+
+
+
+
