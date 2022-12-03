5e4d3dc08113c5cc6bc316bf667bbc9367e346a3
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-03 23:30:28 +0530

    Practice _ 04

---
+++ 17_practice_04.py
@@ -0,0 +1,9 @@
+# Write a program to format the following letter using escape sequences
+#   characters. 
+#          letters = "Dear Sumit ,This Python course is nice.Thanks!"
+
+letter = "Dear Sumit,This Python course is nice! Thanks!"
+print(letter) 
+
+formatted_letter = "Dear Sumit,\n\tThis python course is nice!\nThanks!"
+print(formatted_letter)
