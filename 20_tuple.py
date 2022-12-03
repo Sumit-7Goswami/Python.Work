9bb8255bf83c406e00fe11b32a42749c78186126
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-03 23:33:20 +0530

    Tuple

---
+++ 20_tuple.py
@@ -0,0 +1,21 @@
+# Creating a tuple using ()
+t = (1,2,4,5)
+
+# printing the elements of a tuple 
+print(t[0])
+
+# "Cannot update the value of a tuple , update nhi krr skte haaaa...print krrr skte hh bss 
+t[0] = 34  #throws an error
+
+
+# kuch example dekh lee bhai niche 
+t = (1,2,4,5)
+t1 = ()
+print(t1)  # on run this code we get () empty tuple 
+
+
+#
+t = (1,2,4,5)
+# t1 = (1)  #wrong way to declare a Tuple with Single element 
+t1 = (1,  ) #tuple with Single element 
+print(t1)
