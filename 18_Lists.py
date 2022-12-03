ad96fb539b3fc605cd5c1013989300d553dda76f
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-03 23:31:49 +0530

    Lists

---
+++ 18_Lists.py
@@ -0,0 +1,42 @@
+a = [1,2,3,4,5]
+print(a)
+
+# if we have to take the first value of 'a' let suppose we want
+# to take 1 ,so for that we do " Print(a[0])  "see example below we get 1 
+'''list m be index zero se start hoti hai''' 
+a = [1,2,4,56,6]
+print(a[0]) 
+
+'''another example'''
+a = [1,2,4,56,6]
+print(a[3]) 
+
+a = [1,2,4,56,6]
+print(a[5])
+'''now if we run this code we get error , because there no element'''
+###**** NOW we can also change the list
+# Create a list using []
+a = [1,2,4,56,6]
+# print the list using print() functions
+a[0] = 48   #change the value of list using [point 21] 
+print(a)  
+
+
+
+
+# We can create a list with items of different types
+c = [66,"Sumit","Lion" , 12.3 , True]
+print(c)
+
+
+
+# List Slicing
+friends = ["Rohan","Harry","Tom" , "Sam" ,"Divya" , 48]
+print(friends[0:4])
+
+#example
+friends = ["Rohan","Harry","Tom" , "Sam" ,"Divya" , 48]
+print(friends[-4:])
+
+
+
