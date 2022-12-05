709da321886df3dd5727275896c9f40fc68e8b77
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-05 23:25:05 +0530

    Add files via upload

---
+++ 07_practice _set.py
@@ -0,0 +1,48 @@
+#Q write A Python program to add two numbers
+ 
+a = 44
+a += 6
+print(a)  
+"we can do like this also as given below"
+a = 50 
+b = 10 
+print("The sum of a and b is " ,a+b)
+
+#Q write a python program to find remainder when a number
+# is divided by 2 . 
+a = 45 
+b = 2 
+print(a%b) # here after run this code we get 1 ,because (%) this sign we called modulus,and it gives remainder ,generally it is used to define even and odd numbers
+ 
+
+'''Or'''
+a = 45 
+b = 15 
+
+print("The remainder when a is divided by b is ", a%b) 
+'''here we got answer 0 because when we divide 45 by 15 we
+got answer 3 and remainder get zero''' 
+
+a = 458
+b = 5 
+print("The remainder when a is divided by b is" ,a%b )
+
+#
+#Q  Check the type of the variable assigned using input ()function
+a = input("Enter a number here:")
+"Enter a number here = 69"  
+print(type(a)) #we will run this code in immediate mode ex;WINDOW POWERSHELL 
+
+
+#Q  use comparison operators to find out whether a given variable 'a' is greater than 'b' or not 
+# Take  a = 34 and b = 80 
+a = 34
+b = 80
+print(a>b) 
+'''we can use window powershell for this Q.we get boolean as true 
+or false '''
+
+
+
+
+ 
