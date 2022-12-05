55d534b76577b21ffe19aaf0c3429b89429725d1
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-05 23:24:22 +0530

    Add files via upload

---
+++ 05_input_function.py
@@ -0,0 +1,13 @@
+a = input("Enter your name : ")
+print(type(a))
+
+####
+a = input("Enter your name : ")
+a = int(a) #convert a to an integer(if possible) 
+print(type(a)) #we cannot convert a SUMIT into number
+#it does not make any sense 
+
+#another anti-concept
+a = input("Enter a number: ")
+a = int(a) #convert a to an integer 
+print(type(a))
