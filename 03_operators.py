0048370c7aa35302ddc9e783e0a0217493646ac6
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-05 23:25:37 +0530

    Add files via upload

---
+++ 03_operators.py
@@ -0,0 +1,53 @@
+# Arithmetic Operators
+
+
+
+print( "The value of 3+4 is ", 3+4)
+print("The value of 3*4 is" , 3*4) 
+print("The value of 3-4 is ", 3-4)
+print("The value of 3/4 is ", 3/4)
+
+
+# Assigment Operators
+a = 4 
+a += 2
+print(a)
+
+b = 25 
+b += 5 
+print(b)
+
+name = "Sumit"
+name += "Goswami"
+print(name)
+
+d = 17 
+d -= 7 
+print(d)
+
+# Comparison Operators
+a = (4>7)
+print(a)
+
+a = (4<7)
+print(a)
+
+b = (14>=7)
+print(b)
+
+c = (17!=7)  
+'''here (!) this sign denotes 'not equal to' ...yes so on run
+this code we get , TRUE because 17 is not equal to '''
+print(c)
+...
+...
+#LOGICAL OPERATORS
+bool1 = True 
+bool2 = False 
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 or bool2 is" ,(bool1 or bool2))
+print("The value of not bool2 is",(not bool2))
+
+
+
+'''comparison operator boolean return krta hai '''
