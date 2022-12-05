cf066ee32217b56ed3af6a9ea652707fdd72cf03
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-05 23:27:27 +0530

    Add files via upload

---
+++ 01_variable.py
@@ -0,0 +1,24 @@
+a = "Ben Stokes"
+# we can also use like this 
+# a = 'Harry'
+# a = '''Harry'''
+b = 44 
+c = 79.99 
+d = False
+e = None
+
+#printing the variable 
+print (a)
+print(c)
+
+#printing the type of variables
+print(type(a))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+'''here we know a b c all ere variables and their values
+in their variable'''
+# data types - a type of information which we stores
+# as  data 
