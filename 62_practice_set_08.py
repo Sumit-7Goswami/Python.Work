473686934dff97f16de03101a8179506e41934e6
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-09 23:54:16 +0530

    Add files via upload

---
+++ 62_practice_set_08.py
@@ -0,0 +1,17 @@
+# Write a program to print the following star pattern: 
+#     *
+#     * *
+#     * * *            for n = 3
+ 
+n = 3
+for i in range(3):
+    print("*" * (i+1))
+
+
+
+
+
+
+
+
+
