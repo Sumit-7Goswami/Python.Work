b3676e46eb149385db538b05e1a5734eecd7e623
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-13 23:17:22 +0530

    Add files via upload

---
+++ 69_practice_02.py
@@ -0,0 +1,8 @@
+##Question---> write a python program using function to convert celsius to  fahrenheit
+'''       ( celsius *(9/5)) + 32         '''
+
+def frah(cel):
+    return (cel * (9/5)) + 32
+print(frah(24))    
+print(frah(32))
+print(frah(21))
