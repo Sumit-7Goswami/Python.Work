f6042b1837e02e5f9a78bd1abf458abf416ac771
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-14 23:44:42 +0530

    Add files via upload

---
+++ 72_practice_05.py
@@ -0,0 +1,11 @@
+# write a python function to print first n lines of the given following pattern             #                                                          
+#   * * *
+#   * *
+#   *                      for n = 3
+
+
+n = 3
+# i = 0
+for i in range(n):
+    print("*" * (n-i))      #'''print * (n-i) times '''
+ 
