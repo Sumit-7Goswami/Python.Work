dd5a486a8cd79ee0d1780533407ad27d680d6d2b
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-15 23:46:51 +0530

    Add files via upload

---
+++ 78_03_write.py
@@ -0,0 +1,12 @@
+f = open('another.txt','w')
+f.write("Please2 write this to the file ")
+f.close() ## it automatically make the file "another.txt",we don't need to make this specific
+
+#######
+f = open('another.txt','a')
+f.write("I am apppending")
+f.close()
+
+
+
+
