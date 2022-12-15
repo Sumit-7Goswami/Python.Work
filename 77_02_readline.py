6baf266197f1cdf801791810c3cdeb36254c3a0d
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-15 23:46:24 +0530

    Add files via upload

---
+++ 77_02_readline.py
@@ -0,0 +1,19 @@
+f = open('sample.txt')  ## if we not write 'r' ,then it autmotically read ,we call it default
+#   read first line 
+data = f.readline()
+print(data)
+
+#  read 2nd line 
+data = f.readline()
+print(data)
+
+# Read 3rd line
+data = f.readline()
+print(data)
+
+# Read fourth line... and soo on
+data = f.readline()
+print(data)
+f.close
+
+
