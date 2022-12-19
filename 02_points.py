64894a625b6aaeb340bb0bd2d7eb60b2ecddd964
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-19 23:37:16 +0530

    Add files via upload

---
+++ 02_points.py
@@ -0,0 +1,11 @@
+a = 44 
+A = 94
+print(a)
+
+
+#now see below if we take the name of variables is same 
+a = 95 
+a = 45.6
+print(a)
+'''so if we run this code the answer will be 45.6  
+because it comes with order  and computer se only last variables '''
