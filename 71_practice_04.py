eee69ea2cb79851d723670a8d3a08cf5550915d3
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-21 23:55:54 +0530

    Add files via upload

--- 71_practice_04.py
+++ 71_practice_04.py
@@ -1,15 +1,15 @@
 # write a recursive function  to calculate the sum of first n natural numbers
 ##################  method 1
+num = int(input())
 def recurSum(n):
     if n <= 1:
         return n
     return n + recurSum(n - 1)
-n = 5
-print(recurSum(n))
+print(recurSum(num))
 
 ###############     method 2
-def recurSum(n):
-    if n<=1:
-        return n
-    return n + recurSum(n-1)
-print(recurSum(5))         
+# def recurSum(n):
+#     if n<=1:
+#         return n
+#     return n + recurSum(n-1)
+# print(recurSum(5))         
