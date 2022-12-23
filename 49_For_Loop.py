acfa49ece7b650c736a50a1feafa5cf725478731
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2022-12-23 23:17:58 +0530

    Add files via upload

---
+++ 49_For_Loop.py
@@ -0,0 +1,26 @@
+# For LOOP ---> a For Loop is used to iterate through a sequence like
+#               list ,tuple,or string [iterables]
+
+''' The syntax of a loop looks like this :  '''
+
+ 
+
+
+l = [1,7,8]
+for item in l :
+    print(l)   # print 1,7,8
+
+
+# Quiz----> 
+fruits = ['Banana','Watermelon','Grapes','Mangoes'] 
+for item in fruits:
+    print(item) 
+
+'''while loop mee itna likna pdaa tha dekh loo ,,isliye apnn for loop mai too easy too 
+    to execute and understand'''
+
+
+
+
+
+
