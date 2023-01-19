8bec236910d8f2d02d631b37ed8fa3e436f0320e
Sumit Goswami <114800598+Sumit-7Goswami@users.noreply.github.com>
2023-01-19 23:46:58 +0530

    Delete class16-exercise2-CipherSchool.py

--- class16-exercise2-CipherSchool.py
+++
@@ -1,6 +0,0 @@
-#Ask user name and print back user name in reverse order:
-#  Note------>>> try to make your program in 2 lines using string formatting
-
-
-user_name = input("")
-print(user_name[::-1])
